01dd478588821ad6f25bc96b8534bb745c10d58f
AsciiTable <jessicawei9@gmail.com>
2022-05-25 18:56:32 -0700

    + Dove icons and init behavior

--- GAME-252-Project-3-Orphaned-Dove-Egg/Assets/Scripts/DataManager.cs
+++ GAME-252-Project-3-Orphaned-Dove-Egg/Assets/Scripts/DataManager.cs
@@ -17,7 +17,7 @@ public class DataManager : MonoBehaviour
     private static Vector2 treePosition = Vector2.zero;
 
     // Start is called before the first frame update
-    void Start(){
+    void Awake(){
         if (runDefault) {
             RunDefaultData();
         }
--- GAME-252-Project-3-Orphaned-Dove-Egg/Assets/Scripts/DoveBehavior.cs
+++ GAME-252-Project-3-Orphaned-Dove-Egg/Assets/Scripts/DoveBehavior.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class DoveBehavior : MonoBehaviour
 {
+    private enum NestState { 
+        Mom,
+        Dad,
+        Both,
+        None
+    }
+
+    private Vector2 nestingLocation;    // The location of the nest
+    private float timeBothAway;         // The amount of time both parents have been away from the nest for
+    private NestState ns;               // Current nest state
+
+
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        nestingLocation = DataManager.GetTreePosition();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void SwitchStates(NestState newState) { 
         
     }
 }
